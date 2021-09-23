05a9e4cd34c881c60c5af180bddc812ce324bdde
averycordle <77205707+averycordle@users.noreply.github.com>
2021-09-23 11:06:36 -0400

    Add files via upload

---
+++ numList.py
@@ -0,0 +1,22 @@
+numList = [5, 15, 84, 3, 14, 2, 8, 10, 14, 25]
+
+count = numList.count(14)
+print(count)
+
+
+numList.reverse()
+print(numList)
+
+
+numList.sort()
+print(numList)
+
+
+foodList = ["orange", "apple", "Pear", "banana"]
+foodList.sort()
+print(foodList) 
+
+
+print(min(numList))
+
+
